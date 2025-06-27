4185c76f758d3f233527bf27a1ea0aa8fa242ee1
mintdart <96025197+mintdart@users.noreply.github.com>
2025-06-27 23:16:07 +0900

    update

--- sst.config.ts
+++ sst.config.ts
@@ -2,15 +2,26 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-  app(input) {
-    return {
-      name: "defillama-app",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
-      home: "aws",
-    };
-  },
-  async run() {
-    new sst.aws.Nextjs("MyWeb");
-  },
-});
+	app(input) {
+		return {
+			name: 'defillama-app',
+			removal: input?.stage === 'production' ? 'retain' : 'remove',
+			protect: ['production'].includes(input?.stage),
+			home: 'aws'
+		}
+	},
+	async run() {
+		const vpc = new sst.aws.Vpc('MyVpc')
+		const cluster = new sst.aws.Cluster('MyCluster', { vpc })
+
+		new sst.aws.Service('MyService', {
+			cluster,
+			loadBalancer: {
+				ports: [{ listen: '80/http', forward: '3000/http' }]
+			},
+			dev: {
+				command: 'npm run dev'
+			}
+		})
+	}
+})
